3df365a19979055695777f95b81cfa29ac09e994
Grigory-Rylov <grishberg@gmail.com>
2020-06-13 17:35:51 +0300

    Added text field

--- app/src/main/java/com/github/grishberg/customsuggestview/MainActivity.kt
+++ app/src/main/java/com/github/grishberg/customsuggestview/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.github.grishberg.customsuggestview
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,34 @@ class MainActivity : AppCompatActivity() {
         // to start animation from non empty suggest
         //suggestView.setInitialSuggests(mutableListOf("test strings 01234", "another strings 0923423", "yet another st 0934"))
 
+        val editText = findViewById<EditText>(R.id.editText)
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) = Unit
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) = Unit
+
+            override fun onTextChanged(
+                s: CharSequence,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                suggestView.setSuggests(
+                    mutableListOf(
+                        s.toString(),
+                        s.toString(),
+                        s.toString(),
+                        s.toString(),
+                        s.toString()
+                    )
+                )
+            }
+        })
         suggestView.setSuggests(
             mutableListOf(
                 "test string",
